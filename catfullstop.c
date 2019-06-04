cc10408db5c48e6e6b1285d292b812e2b5f6c0c2
Lavanya <45381239+lavanyaa3216@users.noreply.github.com>
2019-06-04 12:55:42 +0530

    Update catfullstop.c

--- catfullstop.c
+++ catfullstop.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-	char a[50];
+	char a[50];int i=0;
 	scanf("%s",a);
-	strcat(a,".");
+	//strcat(a,".");
+	while(a[i]!='/0')
+	{
+		i++;
+	}
+	a[i]='.';
 	printf("%s",a);
 }
